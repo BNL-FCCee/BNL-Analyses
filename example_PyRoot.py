bcd446ab470aa68d7dd2776a1c8ca49de80e2169
Christian Weber <cweber@bnl.gov>
2023-09-19 17:15:11 -0400

    Make an example on how to access truth information

--- example_PyRoot.py
+++ example_PyRoot.py
@@ -6,11 +6,22 @@ if __name__ == '__main__':
 
 
     # Open the input file
-    input_file = ROOT.TFile.Open("most_recent_ScottSamples/hinvjj_500.root", "READ")
+    inputFileLocation = "/gpfs/mnt/atlasgpfs01/usatlas/data/gdamen/FCC/CLICPerformance/Hinv_studies/reconstruction/hinvee_runs/final/hinvee_380GeV.root"
+    input_file = ROOT.TFile.Open(inputFileLocation, "READ")
 
     event_collection = input_file.Get("events")
 
+
+    event_collection.GetListOfBranches()
+
+    print( "Available branches >>>>>>>>")
+    for branch in event_collection.GetListOfBranches(): print(branch.GetName())
+    print( "<<<<<<<< Available branches ")
+
+
     num_events = event_collection.GetEntries()
+
+    #import pdb; pdb.set_trace() # import the debugger and instruct it to stop here
 
     #"TightSelectedPandoraPFOs"
 
@@ -19,17 +30,26 @@ if __name__ == '__main__':
 
     for entry in event_collection:
 
-        for x in entry.TightSelectedPandoraPFOs: x.type
+
+        MCParticles = entry.MCParticles # this is a C++ vector, one can index it
+
+
+
+        for index, particle in enumerate(MCParticles): 
+            print( index, particle.PDG, particle.mass)
+
+        #for x in entry.TightSelectedPandoraPFOs: x.type
+
+
 
         import pdb; pdb.set_trace() # import the debugger and instruct it to stop here
 
 
-    # Loop over the events
-    for event_index in range(num_events):
-        # Get the event object
-        #event = event_collection[event_index]
-
-        AAA = event_collection.GetEntry(event_index)
+    ## Loop over the events
+    #for event_index in range(num_events):
+    #    # Get the event object
+    #    #event = event_collection[event_index]
+    #    AAA = event_collection.GetEntry(event_index)
 
 
 
